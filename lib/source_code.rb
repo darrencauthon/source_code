a81a2e6406505821e3541f4aa03d95be80c3319d
Darren Cauthon <darren@cauthon.com>
2015-06-26 16:45:03 -0500

    First bit of source code extracted from a method.

--- lib/source_code.rb
+++ lib/source_code.rb
@@ -2,5 +2,6 @@ require "source_code/version"
 
 class Method
   def source_code
+    File.read(source_location[0]).lines[source_location[1]-1...source_location[1] + 2].join
   end
 end
--- spec/source_code_spec.rb
+++ spec/source_code_spec.rb
@@ -2,6 +2,7 @@ require_relative 'spec_helper'
 
 class AClassForTesting
   def hey
+    1 + 1
   end
 end
 
@@ -11,6 +12,15 @@ describe :source_location do
 
   it "should monkey-patch source_code on to Method" do
     method.respond_to?(:source_code).must_equal true
+  end
+
+  it "should report back the contents of the method" do
+    method.source_code.must_equal <<EOF
+  def hey
+    1 + 1
+  end
+EOF
+      
   end
 
 end
