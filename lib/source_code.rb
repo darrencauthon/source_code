9d4e281ff0eaa0eb947d724e5d099016c57824d9
Darren Cauthon <darren@cauthon.com>
2015-06-29 06:51:18 -0500

    The ending point is a line with matching indentation.

--- lib/source_code.rb
+++ lib/source_code.rb
@@ -2,6 +2,20 @@ require "source_code/version"
 
 class Method
   def source_code
-    File.read(source_location[0]).lines[source_location[1]-1...source_location[1] + 2].join
+    file = source_location[0]
+    starting_point = source_location[1] - 1
+
+    lines = File.read(file).lines
+    indentation = lines[starting_point].length - lines[starting_point].lstrip.length
+
+    end_point = starting_point + 1
+    end_indentation = lines[end_point].length - lines[end_point].lstrip.length
+
+    while end_indentation != indentation
+      end_point += 1
+      end_indentation = lines[end_point].length - lines[end_point].lstrip.length
+    end
+    end_point += 1
+    File.read(file).lines[starting_point...end_point].join
   end
 end
