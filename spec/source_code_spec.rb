6eacc4cabe58d2d312fe73a68564ca3ac5d09017
Darren Cauthon <darren@cauthon.com>
2015-06-28 19:52:30 -0500

    Another failing test.

--- spec/source_code_spec.rb
+++ spec/source_code_spec.rb
@@ -6,21 +6,35 @@ class AClassForTesting
   end
 end
 
+class AnotherFileForTesting
+  def do_some_math
+    1 + 1
+    2 + 3
+    4 + 5
+    5 + 10
+  end
+end
+
 describe :source_location do
 
-  let(:method) { AClassForTesting.new.method(:hey) }
+  describe "first example" do
+    let(:method) { AClassForTesting.new.method(:hey) }
 
-  it "should monkey-patch source_code on to Method" do
-    method.respond_to?(:source_code).must_equal true
-  end
+    it "should monkey-patch source_code on to Method" do
+      method.respond_to?(:source_code).must_equal true
+    end
 
-  it "should report back the contents of the method" do
-    method.source_code.must_equal <<EOF
-  def hey
+    it "should report back the contents of the method" do
+      method.source_code.must_equal <<EOF
+  def do_some_math
     1 + 1
+    2 + 3
+    4 + 5
+    5 + 10
   end
 EOF
-      
+    end
+
   end
 
 end
